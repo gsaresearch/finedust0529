690e1f69f5234c656f473564fdc7245686b2f9ba
gsaresearch <56109512+gsaresearch@users.noreply.github.com>
2019-11-13 17:15:45 +0900

    Update finedust0529.js

--- finedust0529.js
+++ finedust0529.js
@@ -1,3 +1,22 @@
+function setup()
+{
+  let botton1, button2;
+  button1 = createButton('pm2.5');
+  button1.position(10, 100);
+  button1.mouseClicked(button1clicked);
+  button2 = createButton('pm10');
+  button2.position(10, 130);
+  button2.mouseClicked(button2clicked);
+}
+function button1clicked()
+  {
+    mode = 1;
+  };
+function button2clicked()
+  {
+    mode = 2;
+  };
+
 let graph = function(g)
 {
   let valueX = 0;
